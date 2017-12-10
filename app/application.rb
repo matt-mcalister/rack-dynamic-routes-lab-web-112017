d0bfd005b3cfc0bc6d8135c9998f054087ed7cf7
Matt McAlister <matt.mcalister93@gmail.com>
2017-12-10 16:39:46 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match("/items/")
+      item = req.path.split("/items/").last
+
+      if Item.all.any? {|i| i.name == item}
+        price = Item.all.find {|i| i.name == item}.price
+        resp.write "#{price}"
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
